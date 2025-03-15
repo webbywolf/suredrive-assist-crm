8a78bbe632ee5492f7493c7485ac2e70c745e06c
vinaycreative <majorcoderr@gmail.com>
2025-03-15 09:05:15 +0530

    fix: type validation

--- app/(private)/partners/[slug]/page.tsx
+++ app/(private)/partners/[slug]/page.tsx
@@ -3,13 +3,13 @@ import ExistingPartners from "@/sections/partners/view-existing-partners"
 import React from "react"
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
-  const { slug } = await params
-  if (slug === "add") {
-    return <AddNewPartner />
-  }
-  if (slug === "all") {
-    // return <AddNewUser />
-    return <ExistingPartners />
-  }
+  // const { slug } = await params
+  // if (slug === "add") {
+  //   return <AddNewPartner />
+  // }
+  // if (slug === "all") {
+  //   // return <AddNewUser />
+  //   return <ExistingPartners />
+  // }
   return <div className="h-[500px] w-full div-center">Relax!! I'm working on it.</div>
 }
